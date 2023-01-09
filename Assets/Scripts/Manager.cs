3d9a79b6c415db42ebc78323dcaa91b8bbcc48bc
laporte727 <ecole.la.porte@gmail.com>
2023-01-09 17:06:17 +0900

    3d data

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -8,36 +8,38 @@ public class Manager : MonoBehaviour
     void Start()
     {
         GameObject obj0 = GameObject.Find("pointer");
-       
+        obj0.transform.LookAt(new Vector3(0, 100, 0));
+
+
         GameObject obj1 = Instantiate(obj0);
-        obj1.name = "pointer1";
-        obj1.transform.position = new Vector3(-10, 0, 0);
-        obj1.transform.LookAt(new Vector3(0, 0, 50));
+        obj1.name = "Kabul";
+        obj1.transform.position = new Vector3(29, 57, 77);
+        //obj1.transform.LookAt(new Vector3(0, 100, 0));
     
 
         GameObject obj2 = Instantiate(obj0);
-        obj2.name = "pointer2";
-        obj2.transform.position = new Vector3(-5, 0, 0);
-        obj2.transform.LookAt(new Vector3(0, 0, 50));
+        obj2.name = "Algiers";
+        obj2.transform.position = new Vector3(80, 60, 4);
+        //obj2.transform.LookAt(new Vector3(0, 100, 0));
         
 
         GameObject obj3 = Instantiate(obj0);
-        obj3.name = "pointer3";
-        obj3.transform.position = new Vector3(5, 0, 0);
-        obj3.transform.LookAt(new Vector3(0, 0, 50));
+        obj3.name = "Luanda";
+        obj3.transform.position = new Vector3(96, 15, 23);
+        //obj3.transform.LookAt(new Vector3(0, 100, 0));
         
 
 
         GameObject obj4 = Instantiate(obj0);
-        obj4.name = "pointer4";
-        obj4.transform.position = new Vector3(10, 0, 0);
-        obj4.transform.LookAt(new Vector3(0, 0, 50));
+        obj4.name = "Sydney";
+        obj4.transform.position = new Vector3(-73, -56, -40);
+        //obj4.transform.LookAt(new Vector3(0, 100, 0));
 
 
         GameObject obj5 = Instantiate(obj0);
-        obj5.name = "pointer5";
-        obj5.transform.position = new Vector3(0, 5, 0);
-        obj5.transform.LookAt(new Vector3(0, 0, 50));
+        obj5.name = "Tokyo";
+        obj5.transform.position = new Vector3(-62, 58, 53);
+        //obj5.transform.LookAt(new Vector3(0, 100, 0));
     }
 
     // Update is called once per frame
